a07c6267bab643e3b31b0aead84272a26700839d
Martynas Pumputis <m@lambda.lt>
2023-09-27 07:31:45 +0200

    Update
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

---
+++ nvim/after/ftplugin/c.lua
@@ -0,0 +1,7 @@
+vim.opt.tabstop=8
+vim.opt.shiftwidth=8
+vim.opt.softtabstop=8
+vim.opt.textwidth=80
+vim.opt.expandtab=false
+vim.opt.cindent=true
+vim.opt.cinoptions={':0','l1','t0','g0','(0'}
--- nvim/lua/brb/set.lua
+++ nvim/lua/brb/set.lua
@@ -29,14 +29,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
--- TODO(brb) move to separate plugin and autocmd
-if string.match(vim.api.nvim_buf_get_name(0), "/bpf/") then
-  vim.opt.tabstop=8
-  vim.opt.shiftwidth=8
-  vim.opt.softtabstop=8
-  vim.opt.textwidth=80
-  vim.opt.expandtab=false
-  vim.opt.cindent=true
-  vim.opt.cinoptions={':0','l1','t0','g0','(0'}
-end
